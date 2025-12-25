db6c1f94edd5fcacc5a1e12750619a3da3fbe5e6
Z <z-jinke@qq.com>
2025-12-25 13:18:07 +0800

    Update iPinfo.js

--- Js/iPinfo.js
+++ Js/iPinfo.js
@@ -11,14 +11,42 @@ if (typeof $argument !== "undefined") {
 }
 
 const countryMap = {
-    "HK": "香港","TW": "台湾","KR": "韩国","JP": "日本","DE": "德国","FR": "法国",
-    "GB": "英国","US": "美国","SG": "新加坡","AU": "澳大利亚","CA": "加拿大",
-    "RU": "俄罗斯","IN": "印度","IT": "意大利","ES": "西班牙","BR": "巴西",
-    "NL": "荷兰","CH": "瑞士","SE": "瑞典","NO": "挪威","DK": "丹麦",
-    "FI": "芬兰","PL": "波兰","UA": "乌克兰","MX": "墨西哥",
-    "AE": "阿联酋","SA": "沙特阿拉伯","TR": "土耳其","AR": "阿根廷",
-    "ZA": "南非","NZ": "新西兰","MY": "马来西亚","TH": "泰国",
-    "PH": "菲律宾","VN": "越南","ID": "印度尼西亚"
+    "HK": "香港",
+    "TW": "台湾",
+    "KR": "韩国",
+    "JP": "日本",
+    "DE": "德国",
+    "FR": "法国",
+    "GB": "英国",
+    "US": "美国",
+    "SG": "新加坡",
+    "AU": "澳大利亚",
+    "CA": "加拿大",
+    "RU": "俄罗斯",
+    "IN": "印度",
+    "IT": "意大利",
+    "ES": "西班牙",
+    "BR": "巴西",
+    "NL": "荷兰",
+    "CH": "瑞士",
+    "SE": "瑞典",
+    "NO": "挪威",
+    "DK": "丹麦",
+    "FI": "芬兰",
+    "PL": "波兰",
+    "UA": "乌克兰",
+    "MX": "墨西哥",
+    "AE": "阿联酋",
+    "SA": "沙特阿拉伯",
+    "TR": "土耳其",
+    "AR": "阿根廷",
+    "ZA": "南非",
+    "NZ": "新西兰",
+    "MY": "马来西亚",
+    "TH": "泰国",
+    "PH": "菲律宾",
+    "VN": "越南",
+    "ID": "印度尼西亚"
 };
 
 function maskIPFn(ip) {
@@ -44,7 +72,7 @@ function parseRisk(html) {
     return riskLevel(risk);
 }
 
-const title = "使用节点";
+const title = "当前节点";
 const defaultColor = "#00DFFF";
 const errorColor = "#FF3B30";
 const iconName = "globe.asia.australia.fill";
