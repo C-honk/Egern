4cd0ef599f8000830c09a646b3ef4e715a211043
Z <z-jinke@qq.com>
2026-01-17 22:20:12 +0800

    Update NodePanel.js

--- Js/NodePanel.js
+++ Js/NodePanel.js
@@ -1,33 +1,38 @@
 const mask = $argument.includes("mask=true");
 
-$httpClient.get({url: "http://ip-api.com/json/?lang=zh-CN",timeout: 5000},(err, resp, body) => {
-    let content,iconColor;
-    if (err || !resp || resp.status !== 200) {
-        content = `HTTP${resp?.status || "请求失败"}`;
-        iconColor = "#FF4C4C";
-    } else {
-        let IP = JSON.parse(body);
-        let ipAddr = IP.query;
-      
-        if (mask) {
-            if (ipAddr.includes(".")) {
-                let parts = ipAddr.split(".");
-                parts[3] = "***";
-                ipAddr = parts.join(".");
-            } else if (ipAddr.includes(":")) {
-                let parts = ipAddr.split(":");
-                parts[parts.length - 1] = "***";
-                ipAddr = parts.join(":");
+$httpClient.get(
+    { url: "http://ip-api.com/json/?lang=zh-CN", timeout: 5000 },
+    (err, resp, body) => {
+        let content, iconColor;
+
+        if (err || !resp || resp.status !== 200) {
+            content = `HTTP${resp?.status || "请求失败"}`;
+            iconColor = "#FF4C4C";
+        } else {
+            let IP = JSON.parse(body);
+            let ipAddr = IP.query;
+
+            if (mask) {
+                if (ipAddr.includes(".")) {
+                    let p = ipAddr.split(".");
+                    p[p.length - 1] = "*".repeat(p[p.length - 1].length);
+                    ipAddr = p.join(".");
+                } else if (ipAddr.includes(":")) {
+                    let p = ipAddr.split(":");
+                    p[p.length - 1] = "*".repeat(p[p.length - 1].length);
+                    ipAddr = p.join(":");
+                }
             }
+
+            content = `位置：${IP.country}${IP.countryCode}\n运营：${IP.isp}\nIP址：${ipAddr}`;
+            iconColor = "#2FA3FF";
         }
 
-        content = `位置：${IP.country}${IP.countryCode}\n运营：${IP.isp}\nIP址：${ipAddr}`;
-        iconColor = "#2FA3FF";
+        $done({
+            title: "节点信息",
+            content,
+            icon: "globe.asia.australia.fill",
+            "icon-color": iconColor
+        });
     }
-    $done({
-        title: "节点信息",
-        content,
-        icon: "globe.asia.australia.fill",
-        "icon-color": iconColor
-    });
-});
+);
