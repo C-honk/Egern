e386a8e0929531698f476b85295330dd51c1c206
Z <z-jinke@qq.com>
2026-03-13 08:53:26 +0800

    更新

--- Js/NodePanel.js
+++ Js/NodePanel.js
@@ -1,37 +1,32 @@
-const mask = $argument.includes("mask=ON");
+const mask = $argument?.includes("mask=ON");
 
-const options = {
+$httpClient.get({
   url: "http://ip-api.com/json/?lang=zh-CN",
-  headers: {
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1"
-  },
-  timeout: 5000
-};
+  headers: { "User-Agent": "Mozilla/5.0 (iPhone; CPU OS 17_5 like Mac OS X)" },
+  timeout: 3000
+}, (err, resp, body) => {
 
-$httpClient.get(options, (err, resp, body) => {
 
   if (!resp || resp.status !== 200) {
-    const code = resp ? resp.status : "无响应";
     return $done({
       title: "节点信息",
-      content: `HTTP错误：${code}`,
-      icon: "location.circle.fill",
-      "icon-color": "#FF3A3A"
+      content: `请求失败${resp ? resp.status : "超时"}`
     });
   }
 
   const data = JSON.parse(body);
   let ip = data.query;
 
+
   if (mask) {
-    ip = ip.replace(/([.:])([^.:]+)$/, (m, sep, last) => sep + "*".repeat(last.length));
+    ip = ip.replace(/([.:])([^.:]+)$/, (match, separator, lastPart) => {
+      return separator + "*".repeat(lastPart.length);
+    });
   }
+
 
   $done({
     title: "节点信息",
-    content: `位置：${data.country} ${data.countryCode}\nIP址：${ip}\n运营：${data.isp}`,
-    icon: "location.circle.fill",
-    "icon-color": "#2FA3FF"
+    content: `位置：${data.country} ${data.countryCode}\nIP址：${ip}\n运营：${data.isp}`
   });
-
 });
